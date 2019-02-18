41efb1568e5940d36a1829a9cebe2ed0cd1f7b03
hsonne <hauke.sonnenberg@gmx.de>
2019-02-18 23:35:29 +0100

    Refactor get_radolan_urls, remove duplication

--- R/dwd_radolan.R
+++ R/dwd_radolan.R
@@ -1,5 +1,13 @@
 #' Get URLs to Available Radolan Files on DWD Server
 #'
+#' @param date_start_daily date string (yyyy-mm-dd) of first day of daily
+#'   records. Default: "2006-10-01"
+#' @param date_start_hourly date string (yyyy-mm-dd) of first day of hourly
+#'   records. Default: "2005-06-01"
+#' @param date_end_daily date string (yyyy-mm-dd) of last day of daily
+#'   records. Defaults to the first day of the current month.
+#' @param date_end_hourly date string (yyyy-mm-dd) of last day of hourly
+#'   records. Defaults to \code{date_end_daily}.
 #' @importFrom magrittr %>%
 #' @importFrom kwb.utils resolve
 #' @importFrom lubridate rollback
@@ -7,100 +15,64 @@
 #' @importFrom fs dir_create
 #' @return list with "daily_historical_urls" and "hourly_historical_urls"
 #' @export
-get_radolan_urls <- function() {
-
+get_radolan_urls <- function(
+  date_start_daily = "2006-10-01", date_start_hourly = "2005-06-01",
+  date_end = lubridate::rollback(Sys.Date(), roll_to_first = TRUE)
+) {
 
   base_urls <- kwb.utils::resolve(list(
     base_url = "ftp://ftp-cdc.dwd.de/pub/CDC/grids_germany",
-    daily_historical = "<base_url>/daily/radolan/historical",
-    daily_recent = "<base_url>/daily/radolan/recent/bin",
-    hourly_historical = "<base_url>/hourly/radolan/historical/bin",
-    hourly_recent = "<base_url>/hourly/radolan/historical/bin"
+    daily_radolan = "<base_url>/daily/radolan",
+    hourly_radolan = "<base_url>/hourly/radolan",
+    daily_historical = "<daily_radolan>/historical",
+    daily_recent = "<daily_radolan>/recent",
+    hourly_historical = "<hourly_radolan>/historical/bin",
+    hourly_recent = "<hourly_radolan>/recent/bin"
   ))
 
-  ###############################################################################
-  #### Daily
-  ###############################################################################
-
-  daily_date_start <- "2006-10-01"
-  daily_date_end <- lubridate::rollback(Sys.Date(), roll_to_first = TRUE)
-
-  daily_year <- format(format = "%Y", seq(
-    lubridate::ymd(daily_date_start),
-    lubridate::ymd(daily_date_end),
-    by = 'months'
-  ))
-
-  daily_month <- format(format = "%m", seq(
-    lubridate::ymd(daily_date_start),
-    lubridate::ymd(daily_date_end),
-    by = 'months'
-  ))
-
-  ###############################################################################
-  #### Hourly
-  ###############################################################################
-
-  hourly_date_start <- "2005-06-01"
-  hourly_date_end <-  lubridate::rollback(Sys.Date(), roll_to_first = TRUE)
-
-  hourly_year <- format(format = "%Y", seq(
-    lubridate::ymd(hourly_date_start),
-    lubridate::ymd(hourly_date_end),
-    by = 'months'
-  ))
-
-  hourly_month <- format(format = "%m", seq(
-    lubridate::ymd(hourly_date_start),
-    lubridate::ymd(hourly_date_end),
-    by = 'months'
-  ))
-
-  ###############################################################################
-  #### Saving download links for "hourly" and "daily" in list
-  ###############################################################################
-
-  url_daily_hist_file <- function() {
-
-    tmp_daily_hist <- sprintf(
-      "%s/%s/SF-%s%s.tar.gz",
-      base_urls$daily_historical,
-      daily_year,
-      daily_year,
-      daily_month
-    )
-
-    idx_start <- which(daily_year == "2009" & daily_month == "01")
-    idx_end <- length(tmp_daily_hist)
-
-    tmp_daily_hist[idx_start:idx_end] <- tmp_daily_hist[idx_start:idx_end] %>%
-      stringr::str_replace("SF-", "SF")
-
-    tmp_daily_hist
+  # Define helper functions
+  date_string <- function(date_start, date_end, format) {
+    format(format = format, seq(
+      lubridate::ymd(date_start),
+      lubridate::ymd(date_end),
+      by = 'months'
+    ))
   }
 
-  url_hourly_hist_file <- function() {
+  year_string <- function(start, end) date_string(start, end, format = "%Y")
+  month_string <- function(start, end) date_string(start, end, format = "%m")
 
-    tmp_hourly_hist <- sprintf(
-      "%s/%s/RW-%s%s.tar.gz",
-      base_urls$hourly_historical,
-      hourly_year,
-      hourly_year,
-      hourly_month
+  url_hist_file <- function(years, months, subdir, prefix, year, month = "01") {
+
+    urls <- sprintf(
+      paste0("%s/%s/", subdir, "-%s%s.tar.gz"),
+      base_urls[[paste0(prefix, "_historical")]], years, years, months
     )
 
-    idx_start <- which(hourly_year == "2006" & hourly_month == "01")
-    idx_end <- length(tmp_hourly_hist)
+    indices <- seq.int(which(years == year & months == month), length(urls))
 
-    tmp_hourly_hist[idx_start:idx_end] <- tmp_hourly_hist[idx_start:idx_end] %>%
-      stringr::str_replace("RW-", "RW")
+    urls[indices] <- urls[indices] %>%
+      stringr::str_replace(paste0(subdir, "-"), subdir)
 
-    tmp_hourly_hist
+    urls
   }
 
+  # Return download links for "hourly" and "daily" in a list
   list(
-    daily_historical_urls = url_daily_hist_file(),
-    hourly_historical_urls = url_hourly_hist_file()
+    daily_historical_urls = url_hist_file(
+      years = year_string(date_start_daily, date_end),
+      months = month_string(date_start_daily, date_end),
+      subdir = "SF",
+      prefix = "daily",
+      year = "2009"
+    ),
+    hourly_historical_urls = url_hist_file(
+      years = year_string(date_start_hourly, date_end),
+      months = month_string(date_start_hourly, date_end),
+      subdir = "RW",
+      prefix = "hourly",
+      year = "2006"
+    )
   )
 }
 
