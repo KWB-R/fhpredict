56c1cb3aee0091a805a13905dc5d6f3c4ae2418a
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2019-06-27 11:15:58 +0200

    Read endpoint from environment variable, too.
    Add helper function get_environment_var()

--- R/get_radolan_urls_bucket.R
+++ R/get_radolan_urls_bucket.R
@@ -2,11 +2,12 @@
 
 #' Get URLs to Binary Radolan Filed From Flusshygiene's Amazon Bucket
 #'
-#' This function requires an environment variable "TOKEN_PROD" to be set to the
-#' token that is required to access the "Production Endpoint". Use
+#' This function requires two environment variables to be set:
+#' "ENDPOINT_PROD": Endpoint to the service that returns URLs to Radolan
+#' files, "TOKEN_PROD": token that is required to access the endpoint. Use
 #' \code{usethis::edit_r_environ()} to open the .Renviron file in your home
-#' directory and add a line "TOKEN_PROD=..." where ... is replaced with the
-#' token.
+#' directory and add two lines "ENDPOINT_PROD=..." and "TOKEN_PROD=..." where
+#' ... is replaced with the corresponding values.
 #'
 #' @param from first day considered in format yyyymmdd, e.g. "20190625".
 #'   Default: string representing "yesterday"
@@ -28,17 +29,26 @@ get_radolan_urls_bucket <- function(
   from = format(Sys.Date() - 1, "%Y%m%d"), to = from, time = ""
 )
 {
-  token <- Sys.getenv("TOKEN_PROD")
-
-  if (token == "") {
-    stop("Set the token in environment variable 'TOKEN_PROD'", call. = FALSE)
-  }
-
-  endpoint <- "https://bvq0pskv98.execute-api.eu-central-1.amazonaws.com/prod"
+  endpoint <- get_environment_var("ENDPOINT_PROD")
+  token <- get_environment_var("TOKEN_PROD")
 
   url <- sprintf("%s?from=%s&to=%s&time=%s", endpoint, from, to, time)
 
   response <- httr::GET(url, httr::add_headers("x-api-key" = token))
 
   sapply(httr::content(response, "parsed")$files, "[[", "url")
+}
+
+# get_environment_var ----------------------------------------------------------
+get_environment_var <- function(name)
+{
+  value <- Sys.getenv(name)
+
+  if (! nzchar(value)) {
+    stop(call. = FALSE, sprintf(
+      "Please set the environment variable '%s'", name
+    ))
+  }
+
+  name
 }
