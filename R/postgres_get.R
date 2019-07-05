f3c33ffb9730cfdf6e2f3b97cbc24e4864ac962f
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2019-07-05 17:59:13 +0200

    Add postgres_post(), extract postgres_request()

--- R/postgres_get.R
+++ R/postgres_get.R
@@ -12,24 +12,70 @@
 #'
 postgres_get <- function(path)
 {
+  postgres_request(path, "GET")
+}
+
+# postgres_post ----------------------------------------------------------------
+
+#' Send POST Request to Postgres API
+#'
+#' @param path relative path, e.g. \code{users/3/bathingspots/18/genericInputs}
+#'   to create a generic input for bathing spot of user 3 with ID 18
+#' @param body list with the fields to be set as \code{key = value} pairs, e.g.
+#'   \code{list(name = "lirum larum")}
+#' @return In case of success this function returns what
+#'   \code{httr::content(response, "parsed")$data} returns. In case of failure
+#'   an empty list with attribute \code{response} (containing the response
+#'   object returned by \code{\link[httr]{POST}}) is returned.
+#' @export
+#'
+postgres_post <- function(path, body = NULL)
+{
+  postgres_request(path, "POST", body)
+}
+
+# postgres_request -------------------------------------------------------------
+postgres_request <- function(path, type = "GET", body = NULL)
+{
+  stopifnot(type %in% c("GET", "POST"))
+
   url <- paste0(assert_final_slash(get_environment_var("API_URL")), path)
 
   token <- get_postgres_api_token()
 
   config <- httr::add_headers("Authorization" = paste("Bearer", token))
 
-  response <- httr::GET(url, config = config)
+  response <- if (type == "GET") {
+
+    httr::GET(url, config = config)
+
+  } else if (type == "POST") {
+
+    httr::POST(url, config = config, body = body, encode = "json")
+  }
 
   parsed <- httr::content(response, "parsed")
 
   status <- httr::status_code(response)
 
-  if (status != 200) {
+  if (type == "GET" && status != 200) {
     error <- kwb.utils::defaultIfNULL(parsed$message, "")
-    message(sprintf("Get request '%s' returned with error:\n'%s'", path, error))
+    message(sprintf("GET request '%s' returned with error:\n'%s'", path, error))
     return(structure(list(), response = response))
   }
 
-  parsed
-}
+  if (type == "POST" && status != 201) {
+    error <- kwb.utils::defaultIfNULL(parsed$message, "")
+    message(sprintf("POST request '%s' returned with error:\n'%s'", path, error))
+    return(structure(list(), response = response))
+  }
 
+  if (type == "GET") {
+
+    parsed
+
+  } else {
+
+    parsed$data
+  }
+}
