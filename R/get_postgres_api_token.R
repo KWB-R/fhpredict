80024326908ead8669298da41ccca79f7a5510e2
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2019-07-05 15:10:14 +0200

    Always return a valid token
    Let get_postgres_api_token() check if a stored
    token is valid. If the stored token is not
    valid, let the function request a new token and
    let it replace the stored token with the new
    token.

--- R/get_postgres_api_token.R
+++ R/get_postgres_api_token.R
@@ -3,26 +3,32 @@ get_postgres_api_token <- function()
 {
   file <- "~/.postgres_api_token"
 
-  if (file.exists(file)) {
+  read_token <- function() kwb.utils::catAndRun(
+    sprintf("Reading access token from '%s'", file),
+    readLines(file)
+  )
 
-    token <- kwb.utils::catAndRun(
-      sprintf("Reading access token from '%s'", file),
-      readLines(file)
-    )
+  write_token <- function(token) kwb.utils::catAndRun(
+    sprintf("Writing access token to '%s'", file),
+    writeLines(token, file)
+  )
 
-  } else {
+  new_token <- function() kwb.utils::catAndRun(
+    "Requesting a new access token",
+    if (! is.null(token_data <- request_token())) {
+      kwb.utils::selectElements(token_data, "access_token")
+    }
+  )
 
-    token <- kwb.utils::catAndRun(
-      "Requesting a new access token", {
-        if (! is.null(token_data <- request_token())) {
-          kwb.utils::selectElements(token_data, "access_token")
-        }
-      })
+  # If a token is stored and if it is valid, return the stored token
+  if (file.exists(file) &&
+      is_valid_postgres_api_token(token <- read_token())) {
+    return(token)
+  }
 
-    if (! is.null(token)) kwb.utils::catAndRun(
-      sprintf("Writing access token to '%s'", file),
-      writeLines(token, file)
-    )
+  # If we arrive here, there is no stored token or the stored token is not valid
+  if (! is.null(token <- new_token())) {
+    write_token(token)
   }
 
   token
