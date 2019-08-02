4cb107d6b7b9e78d3d8b955b78202cdd6ff1c7b4
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2019-08-02 18:35:45 +0200

    Add utility function flatten_recursive_list()

--- R/utils.R
+++ R/utils.R
@@ -4,6 +4,24 @@ assert_final_slash <- function(x)
   paste0(gsub("/+$", "", x), "/")
 }
 
+# flatten_recursive_list -------------------------------------------------------
+flatten_recursive_list <- function(x)
+{
+  stopifnot(is.list(x))
+
+  if (length(x) == 0) {
+    return(data.frame())
+  }
+
+  x <- kwb.utils::excludeNULL(x)
+
+  x <- x[! sapply(x, is.list)]
+
+  stopifnot(all(lengths(x) == 1))
+
+  kwb.utils::asNoFactorDataFrame(x)
+}
+
 # get_environment_var ----------------------------------------------------------
 get_environment_var <- function(name)
 {
