69c1ded03983c59e95bcfdfc05226814578171f9
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2019-08-30 16:18:45 +0200

    Add provide_rain_data_for_bathing_spot()
    This function is intended to be launched by the
    User interface. It looks up the time range of
    available measurements and the area assigned to
    the bathing spot. Rain data are loaded, cropped
    and averaged and stored in the Postgres database.

--- inst/extdata/test_radolan_download_and_crop.R
+++ inst/extdata/test_radolan_download_and_crop.R
@@ -1,74 +1,24 @@
-#
-# Read rain data of a time period, crop polygons and average over each cell
-#
+if (FALSE)
+{
+  # Function to be triggered by the user: Provide rain data for the bathing spot
+  system.time(rain_ids <- fhpredict::provide_rain_data_for_bathing_spot(
+    user_id = 3, spot_id = 1441, sampling_time = "1050",
+    date_range = as.Date(c("2008-07-01", "2009-07-01"))
+  ))
 
-user_id <- 3
-spot_id <- 1441 # Kleine Badewiese
-sampling_time <- "1050"
+  # Read rain from database
+  rain <- fhpredict::api_get_rain(user_id, spot_id)
 
-# Read rain data (averaged over cells within  bathing spot related polygon) for
-# a certain time period
-radolan_stack <- fhpredict:::read_radolan_raster_stack(
-  date_from = 20170510,
-  date_to = 20170515,
-  bathing_season_only = TRUE,
-  sampling_time = sampling_time
-)
+  View(rain)
 
-# Show the raster images
-raster::plot(radolan_stack)
+  # What should we do with duplicates?
+  sum(duplicated(kwb.utils::pasteColumns(rain, c("date", "dateTime"))))
 
-# Get metadata about the current bathing spot
-spot <- fhpredict::api_get_bathingspot(spot_id = spot_id)
+  # Delete rain data with given rain record ids
+  fhpredict::api_delete_rain(user_id, spot_id, rain_ids)
 
-# Check the name of the bathing spot
-spot$nameLong
+  # For simplicity reasons: delete all rain data before inserting new data
+  fhpredict::api_delete_rain(user_id, spot_id)
 
-# Let the user select a polygon around the bathing spot using an R Shiny app
-#select_relevant_rain_area(lng = spot$longitude, lat = spot$latitude)
-#area_from_app <- rain_area
-
-# Provide the polygon in the same structure as returned by
-# select_relevant_rain_area()
-area_from_db <- fhpredict:::convert_area_structure(spot_area = spot$area)
-
-#str(area_from_app)
-str(area_from_db)
-
-#area <- area_from_app
-area <- area_from_db
-
-# Crop the polygons from each raster layer
-cropped <- fhpredict:::crop_area_from_radolan_stack(area, radolan_stack)
-
-plot(cropped, add = TRUE)
-
-# Get the mean over all layers for each point on the raster
-aggregated <- raster::cellStats(cropped, stat = mean)
-
-# The day information can be restored from the names of the layers
-dates <- as.Date(substr(names(radolan_stack), 2, 9), format = "%Y%m%d")
-
-rain_df <- data.frame(
-  datum = dates,
-  rain = as.numeric(aggregated) / 10
-)
-
-str(structure(rain_df, units = list(rain = "mm/h")))
-
-# Add rain data frame to the database
-rain_ids <- fhpredict::api_add_rain(user_id, spot_id, rain_df)
-
-# Read rain from database
-rain <- fhpredict::api_get_rain(user_id, spot_id)
-
-# What should we do with duplicates?
-sum(duplicated(kwb.utils::pasteColumns(rain, c("date", "dateTime"))))
-
-# Delete rain data with given rain record ids
-fhpredict::api_delete_rain(user_id, spot_id, rain_ids)
-
-# For simplicity reasons: delete all rain data before inserting new data
-fhpredict::api_delete_rain(user_id, spot_id)
-
-# TODO: Remove only the duplicates, keeping the most current values
+  # TODO: Remove only the duplicates, keeping the most current values
+}
