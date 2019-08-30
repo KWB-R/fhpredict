e6da7605a01386825acc56eaafdf34612b7c9a5e
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2019-08-30 10:09:37 +0200

    Add script to compare model representation sizes

---
+++ inst/extdata/compare_sizes_of_model_representations.R
@@ -0,0 +1,19 @@
+# Model object in R
+models <- list(
+  kwb.flusshygiene.app:::model_grunewaldturm,
+  kwb.flusshygiene.app:::model_kleine_badewiese
+)
+
+sizes <- lapply(models, function(model) {
+  # Save model to binary file (.RData)
+  file_rdata <- tempfile(fileext = ".RData")
+  save(model, file = file_rdata)
+
+  list(
+    size_object = object.size(model),
+    size_rdata = file.info(file_rdata)$size,
+    size_text = object.size(fhpredict:::model_to_text(model))
+  )
+})
+
+str(sizes)
