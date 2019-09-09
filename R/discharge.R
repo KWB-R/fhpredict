b0e345aad2446b5a5a99df4ce49dcb4065c41690
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2019-09-09 17:40:22 +0200

    Add functions to get/add/delete discharge data

---
+++ R/discharge.R
@@ -0,0 +1,66 @@
+# api_add_discharge ------------------------------------------------------------
+api_add_discharge <- function(user_id, spot_id, discharge, comment = NULL)
+{
+  stopifnot(is.data.frame(discharge))
+
+  # Provide vectors of dates and values
+  timestamps <- as.character(
+    #format.POSIXct(tz = "UTC",
+      kwb.utils::selectColumns(discharge, "dateTime")
+    #)
+  )
+
+  values <- kwb.utils::selectColumns(discharge, "discharge")
+
+  result <- lapply(seq_along(values), function(i) {
+    add_timeseries_point_to_database(
+      path = path_discharges(user_id, spot_id),
+      date_string = substr(timestamps[i], 1, 10),
+      time_string = substr(timestamps[i], 12, 19),
+      value = values[i],
+      comment = comment,
+      subject = "discharge"
+    )
+  })
+
+  # Return the ids of the discharge data records
+  unlist(result)
+}
+
+# api_get_discharge ------------------------------------------------------------
+
+#' Get Discharge Data from the Postgres Database Via API
+#'
+#' @param user_id user id
+#' @param spot_id bathing spot id
+#' @export
+#'
+api_get_discharge <- function(user_id, spot_id)
+{
+  api_get_timeseries(
+    path = path_discharges(user_id, spot_id),
+    subject = "discharge"
+  )
+}
+
+# api_delete_discharge ---------------------------------------------------------
+
+#' Delete Discharge Data from Postgres Database Via API
+#'
+#' @param user_id user id
+#' @param spot_id bathing spot id
+#' @param ids optional. Vector of discharge ids. If not given or \code{NULL}
+#'   (the default) all discharge data for the bathing spot are deleted!
+#' @param dbg if \code{TRUE} debug messages are shown
+#' @export
+#'
+api_delete_discharge <- function(user_id, spot_id, ids = NULL, dbg = TRUE)
+{
+  api_delete_timeseries(
+    user_id = user_id,
+    spot_id = spot_id,
+    ids = ids,
+    path_function = path_discharges,
+    subject = "discharge"
+  )
+}
