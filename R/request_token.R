dfe2dce6436ca075ab6d7717314e975ad0ca03ca
hsonne <hauke.sonnenberg@gmx.de>
2019-10-20 14:36:28 +0200

    Try at maximum five times to get a token

--- R/request_token.R
+++ R/request_token.R
@@ -10,9 +10,10 @@
 #'   \item AUTH0_AUDIENCE
 #' }
 #'
+#' @param max_trials maximum number of attempts to request a token
 #' @keywords internal
 #'
-request_token <- function()
+request_token <- function(max_trials = 5)
 {
   url <- get_environment_var("AUTH0_REQ_URL")
 
@@ -25,8 +26,20 @@ request_token <- function()
     grant_type = "client_credentials"
   )))
 
-  # Request a new token
-  response <- httr::POST(url, config = config, body = body, encode = "json")
+  # Initialise loop conditions
+  response <- NULL
+  trial <- 1L
+
+  # Try max_trials-times at maximum to request a token
+  while ((is.null(response) || is_error(response)) && trial < max_trials) {
+
+    response <- kwb.utils::catAndRun(
+      sprintf("Requesting a token (%d/%d)", trial, max_trials),
+      expr = try(httr::POST(url, config = config, body = body, encode = "json"))
+    )
+
+    trial <- trial + 1
+  }
 
   # Get the status code (200 means success)
   status <- httr::status_code(response)
