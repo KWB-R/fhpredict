33a8889c0337c286b8071633444111bab2c85526
hsonne <hauke.sonnenberg@gmx.de>
2019-10-20 14:43:33 +0200

    Use stop_on_request_failure() in is_valid_..._token()

--- R/is_valid_postgres_api_token.R
+++ R/is_valid_postgres_api_token.R
@@ -1,18 +1,19 @@
 # is_valid_postgres_api_token --------------------------------------------------
 is_valid_postgres_api_token <- function(token)
 {
-  url <- get_environment_var("API_URL")
+  response <- httr::GET(
+    url = get_environment_var("API_URL"),
+    config = httr::add_headers(Authorization = paste("Bearer", token))
+  )
 
-  config <- httr::add_headers(Authorization = paste("Bearer", token))
-
-  response <- httr::GET(url, config = config)
+  stop_on_request_failure(response = response)
 
   success <- httr::content(response, "parsed", encoding = "UTF-8")$success
+
   success <- kwb.utils::defaultIfNULL(success, FALSE)
 
   if (! success) {
     message(get_text("invalid_token"))
-    utils::str(httr::headers(response))
   }
 
   success
