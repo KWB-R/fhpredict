7d106c56fec3051b7004d6a3f2d0b47adcbe8104
hsonne <hauke.sonnenberg@gmx.de>
2019-10-20 15:09:14 +0200

    Check for expected status code in is_valid_..._token()

--- R/is_valid_postgres_api_token.R
+++ R/is_valid_postgres_api_token.R
@@ -1,7 +1,20 @@
 # is_valid_postgres_api_token --------------------------------------------------
 is_valid_postgres_api_token <- function(token)
 {
-  content <- safe_postgres_get(path = "", token = token)
+  content <- postgres_get(path = "", token = token)
+
+  if (! is.null(response <- attr(content, "response"))) {
+
+    status_code <- httr::status_code(response)
+
+    # Expected status code for lacking authorisation
+    if (status_code == 401) {
+      return(FALSE)
+    }
+
+    # Unexpected request failure
+    stop_on_request_failure(response = response)
+  }
 
   success <- kwb.utils::defaultIfNULL(content$success, FALSE)
 
