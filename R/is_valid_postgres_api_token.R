8ac5e0f59c6a76213a2d9c1fcc4324b6c1f3fc17
hsonne <hauke.sonnenberg@gmx.de>
2019-10-20 14:58:19 +0200

    Reuse exising functions in is_valid_..._token()

--- R/is_valid_postgres_api_token.R
+++ R/is_valid_postgres_api_token.R
@@ -1,16 +1,9 @@
 # is_valid_postgres_api_token --------------------------------------------------
 is_valid_postgres_api_token <- function(token)
 {
-  response <- httr::GET(
-    url = get_environment_var("API_URL"),
-    config = httr::add_headers(Authorization = paste("Bearer", token))
-  )
+  content <- safe_postgres_get(path = "", token = token)
 
-  stop_on_request_failure(response = response)
-
-  success <- httr::content(response, "parsed", encoding = "UTF-8")$success
-
-  success <- kwb.utils::defaultIfNULL(success, FALSE)
+  success <- kwb.utils::defaultIfNULL(content$success, FALSE)
 
   if (! success) {
     message(get_text("invalid_token"))
