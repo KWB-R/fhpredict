8b9c52b3d5a5180d62f78f7e5d6d20ed51aa761d
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2019-10-22 15:08:15 +0200

    Fix test

--- tests/testthat/test-function-determine_days_to_predict.R
+++ tests/testthat/test-function-determine_days_to_predict.R
@@ -4,7 +4,10 @@ test_that("determine_days_to_predict() works", {
 
   date <- "2019-07-05"
 
-  expect_identical(f(user_id = -1), Sys.Date())
+  expect_identical(
+    f(user_id = -1), seq(as.Date("2019-09-26"), by = 1, length.out = 5)
+  )
+
   expect_identical(f(from = date), as.Date(date))
   expect_identical(f(to = date), as.Date(date))
   expect_identical(f(from = date, to = date), as.Date(date))
