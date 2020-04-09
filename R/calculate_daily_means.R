ed3ed2fe4790455813473b81b279a23f5ba03a4b
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2020-04-09 17:14:38 +0200

    By default, remove NA in mean calculation
    Also, add a NEWS entry

--- R/calculate_daily_means.R
+++ R/calculate_daily_means.R
@@ -1,5 +1,5 @@
 # calculate_daily_means --------------------------------------------------------
-calculate_daily_means <- function(df, date_column = "datum")
+calculate_daily_means <- function(df, date_column = "datum", na.rm = TRUE)
 {
   df[[date_column]] <- as.Date(
     format(kwb.utils::selectColumns(df, date_column), "%Y-%m-%d")
@@ -8,7 +8,7 @@ calculate_daily_means <- function(df, date_column = "datum")
   df %>%
     kwb.utils::hsMatrixToListForm(keyFields = date_column) %>%
     dplyr::group_by(.data$datum, .data$parName) %>%
-    dplyr::summarise(parVal = mean(parVal)) %>%
+    dplyr::summarise(parVal = mean(.data$parVal, na.rm = na.rm)) %>%
     tidyr::pivot_wider(names_from = "parName", values_from = "parVal") %>%
     as.data.frame()
 }
